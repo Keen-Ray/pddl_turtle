58768158274ffb297d7da46460069527bb53a314
Keenan Rhea <keenanrhea@gmail.com>
2021-05-07 17:03:00 -0500

    implimented go_to action

--- include/go_to.h
+++ include/go_to.h
@@ -1,7 +1,9 @@
 #include <ros/ros.h>
+#include <rosplan_action_interface/RPActionInterface.h>
+#include <move_base_msgs/MoveBaseAction.h>
+#include <actionlib/client/simple_action_client.h>
 #include <vector>
-
-#include "rosplan_action_interface/RPActionInterface.h"
+#include <map>
 
 #ifndef PDDL_GO_TO
 #define PDDL_GO_TO
@@ -15,6 +17,19 @@ namespace KCL_rosplan {
 	{
 
 	private:
+		
+		typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
+		typedef struct
+		{
+			double x;
+			double y;
+			double qz;
+			double w;
+		} door_loc;
+
+		std::map<std::string, door_loc> database;
+
+
 
 	public:
 
--- src/go_to.cpp
+++ src/go_to.cpp
@@ -5,18 +5,99 @@ namespace KCL_rosplan {
 
 	/* constructor */
 	GoToInterface::GoToInterface(ros::NodeHandle &nh) {
-		// perform setup
+
+		//get the frame refrence for the points
+
+		// set the door initial position by hard coding
+		// for a bigger project we could build an automated system
+		// to do this
+
+		//TODO extend so that the robot take into account which room it is 
+		//coming from when choosing a nav point
+		door_loc door1;
+		//door1.x     = -7.85;
+		//door1.y     = 0.8;
+		door1.x     = -6.33;
+		door1.y     = 0.15;
+		door1.qz    = 0.707;
+		door1.w     = 0.707;
+
+		door_loc door2;
+		door2.x     = -0.35;
+		door2.y     = -1.15;
+
+		door_loc door3;
+		door3.x     = 0.4;
+		door3.y     = -1.05;
+		door3.qz    = 0.0;
+		door3.w     = 0.1;
+		door_loc door4;
+		door4.x     = 4.6;
+		door3.y     = -1.05;
+		door_loc door5;
+		door5.x     = 1.3;
+		door5.y     = 3.05;
+
+		database.insert(std::pair<std::string, door_loc>("door1", door1));
+		database.insert(std::pair<std::string, door_loc>("door2", door2));
+		database.insert(std::pair<std::string, door_loc>("door3", door3));
+		database.insert(std::pair<std::string, door_loc>("door4", door4));
+		database.insert(std::pair<std::string, door_loc>("door5", door5));
+
 	}
 
 	/* action dispatch callback */
 	bool GoToInterface::concreteCallback(const rosplan_dispatch_msgs::ActionDispatch::ConstPtr& msg) {
 
 		// The action implementation goes here.
+		std::string door = "";
+		std::string from = "";
+		for (int i = 0; i < msg->parameters.size(); i++)
+		{
+			if (msg->parameters[i].key.compare("from") == 0) {
+				from = msg->parameters[i].value;
+			}
+			if (msg->parameters[i].key.compare("d") == 0) {
+				door = msg->parameters[i].value;
+			}
+		}
+		
 
-		// complete the action
-		ROS_INFO("PDDL: (%s) TUTORIAL Action completing.", msg->name.c_str());
-		return true;
+		//tell the action client that we want to spin a thread by default
+  		MoveBaseClient ac("move_base", true);
+
+		//wait for the action server to come up
+		while(!ac.waitForServer(ros::Duration(5.0))) {
+			ROS_INFO("Waiting for the move_base action server to come up");
+		}
+
+  		move_base_msgs::MoveBaseGoal goal;
+
+  		//we'll send a goal to the robot to move 1 meter forward
+  		goal.target_pose.header.frame_id = "map";
+  		goal.target_pose.header.stamp = ros::Time::now();
+
+		door_loc door_info = database.find(door)->second;
+
+  		goal.target_pose.pose.position.x = door_info.x;
+		goal.target_pose.pose.position.y = door_info.y;
+  		goal.target_pose.pose.orientation.z = door_info.qz;
+  		goal.target_pose.pose.orientation.w = door_info.w;
+
+  		ROS_INFO("PDDL: (%s) Sending goal x = %f, y = %f", msg->name.c_str(), door_info.x, door_info.y);
+  		ac.sendGoal(goal);
+
+  		ac.waitForResult();
+
+		if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED) {
+			ROS_INFO("PDDL: (%s) TUTORIAL from: %s to %s completing.", msg->name.c_str(), from.c_str(), door.c_str());
+			return true;
+		} else {
+			ROS_INFO("PDDL: (%s) The base failed to move to %s", msg->name.c_str(), door.c_str());
+			return false;
+		}
 	}
+
 } // close namespace
 
 	/*-------------*/
